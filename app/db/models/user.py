63b976d369bafe1c067ab0650abe60f85c6e57cc
saidaAourras <sa123aouras@gmail.com>
2026-01-20 23:55:11 +0100

    update User model structure

---
+++ app/db/models/user.py
@@ -0,0 +1,21 @@
+from .base import Base
+from sqlalchemy import Column , Integer , String , DateTime 
+from datetime import datetime
+from sqlalchemy.orm import relationship 
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    
+
+    
+    
+   
+    
